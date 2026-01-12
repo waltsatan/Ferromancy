bc0ab3b9cc41c7d060dda76fa003010b9f4ea4da
waltsatan <alan@datax.com>
2026-01-11 21:58:44 -0700

    sliders

--- SolenoidDriver_84/Solenoid84/Solenoid84.ino
+++ SolenoidDriver_84/Solenoid84/Solenoid84.ino
@@ -1,58 +1,33 @@
 
 // GPIO defines -------------------------------------------
-#define SPEED       A3    // Timer frequency setting input (analog)
-//#define CLKOUT      6     // Timer output (TIMER1, OC1A)
-#define LED      0     // Timer output (TIMER1, OC1A)
+#define SPEED       A0    // Timer frequency setting input (analog)
+#define POWER       A1    // Timer frequency setting input (analog)
+
 #define ANALOG  7
-volatile byte state = HIGH; 
 #define BUTTON  8
-// Timer frequency function -------------------------------
-void timerFreq() {
-  OCR1A = 0;//255;//252 - 0;//(analogRead(SPEED)/6); // Yields approx. 16kHz to 48kHz freq range
-}
+
+
+volatile byte state = HIGH; 
+
+ 
 
 // SETUP --------------------------------------------------
 void setup() {
-//	pinMode(CLKOUT, OUTPUT);  // Set pin as output
-	pinMode(LED,OUTPUT);
-	pinMode(ANALOG,OUTPUT);
-	digitalWrite(LED, LOW); 
+
+	 
+	pinMode(ANALOG, OUTPUT);
 	
 	pinMode(BUTTON, INPUT);
 	attachInterrupt(digitalPinToInterrupt(BUTTON), toggle, CHANGE);
   
-	//TCCR1A = _BV(COM1A0) | _BV(WGM11) | _BV(WGM10);
-	//TCCR1B = _BV(CS10) | _BV(CS12) | _BV(WGM12) | _BV(WGM13);
-	//OCR1A = 254;
-	//TIMSK1 |= (1 << OCIE1A);
-	//TCCR1A = 0;
-	//TCCR1B = 0;
-	//TCNT1  = 0;
-	
-	//1500 = 43hz = 23.25
-	//1350 = 47hz = 21.28
-	//1200 = 53hz = 18.87
-	//1050 = 61hz = 16.39
-	
-	//TCCR1B |= (1 << WGM12);
-	////TCCR1B |= (1 << CS12) | (1 << CS10);
-	//TCCR1A =   _BV(WGM10);
-	//TCCR1B =    _BV(CS11) |  _BV(WGM12);
-	//OCR1A = 65535;//0;//255-(16*0);
-	
-	//TCCR1A =   _BV(WGM11) | _BV(WGM10);
-	//TCCR1B = _BV(WGM12) |_BV(WGM13) | _BV(CS12);
-	//OCR1A = 1025; //16000000hz/256hz/60hz  ≈ 1025
-	//OCR1B = 512;
-   
-	
-	 
-	//TIMSK1 |= _BV(OCIE1A);
-	
+	pinMode(SPEED, INPUT);  
+	pinMode(POWER, INPUT);  
 
 	start();
 	
 }
+volatile uint16_t ocr=1535;
+volatile byte power=255;
 
 void start() {
 	cli(); // stop interrupts
@@ -62,12 +37,13 @@ void start() {
 	// set compare match register for 500 Hz increments
 	TIMSK1=0;
 	
+  	
 	//OCR1A = 65535/((1000/  1000   )+1); // 20 ms
-	OCR1A = 65535/((1000/  35   )+1); // 20 ms
-	
+	//OCR1A = 65535/((1000/  30   )+1); // 20 ms
+	OCR1A = ocr;
  // OCR1A = 1;
 	// turn on CTC mode
-	TCCR1B = _BV(WGM12) | _BV(CS11);// | _BV(CS10);
+	TCCR1B = _BV(WGM12) | _BV(CS11);// | _BV(CS10); //8 = 1,000,000/s
 	// Set CS12, CS11 and CS10 bits for 1 prescaler
 	
 	//TCCR1B |= _BV(CS10);
@@ -101,21 +77,32 @@ void toggle() {
 }
 
 void setLED() {
-  if ( active ) {
-		 	if ( state ) {
-			analogWrite(ANALOG,255);
+  
+  //TCNT1  = 0; 
+	if ( active ) {
+		if ( state ) {
+     // TCNT0=0; 
+      analogWrite(ANALOG, power);
+		
+
+      //sei();
 		} else {
-    	  digitalWrite(ANALOG,0);
-			analogWrite(ANALOG,0);
+    	//  digitalWrite(ANALOG,0);
+			analogWrite(ANALOG, 0);
 		}
-	 state=!state;
+		state=!state;
 	} else {
-		digitalWrite(ANALOG,0);
+		analogWrite(ANALOG, 0);
 	}
 
 
 }
+
+
+
 ISR(TIMER1_COMPA_vect) {
+  OCR1A=ocr;
+ 
   setLED();
 	
   
@@ -127,6 +114,31 @@ ISR(TIMER1_COMPA_vect) {
 
 // LOOP ---------------------------------------------------
 void loop() {
+	 
+	//CR1A = map(s,0,1023,0,65535);
+	 int s= analogRead(SPEED);
+	 double ss=s/1023.0;
+	 double p=pow(ss,2); 
+     // int d=map(s,0,1023,1,50);
+      ocr=map(floor(p*1023),20,1000,120,10200);
+    //  ocr=10;
+    
+     int ar=analogRead(POWER);
+     if ( ar > 800 ) {
+       power=255;
+     } else if ( ar < 100 ) {
+        power=0;
+     } else {
+	      power=map(ar,20,1000,0,255);
+     
+     }
+
+     // int d=map(s,0,1023,1,50);
+	//power=map(analogRead(POWER),20,1000,0,255);
+      
+	//OCR1A = 65535/((1000/  30   )+1); // 20 ms
+	//10200 = 65535/(( 1000 / x))
+  //0.15= 1000/x
  // timerFreq();
 }
  
