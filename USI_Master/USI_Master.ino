80cfd36579c3d05d6d732c05c907c71aa4eb6c24
waltsatan <alan@datax.com>
2026-01-15 21:30:15 -0700

    USI

---
+++ USI_Master/USI_Master.ino
@@ -0,0 +1,173 @@
+/*  SPI Master Demo written by Robert Mollik, 2016
+ *  
+ *  This demo uses the SPI protocol on a Arduino Uno R3 / ATmega8 controller
+ *  
+ *  The demo sends out a request ID depending, whether a temperature or humidity signal shall be obtained from the slave. Both signals 
+ *  are datatype float, however get received as 4 bytes and need to be re-assembled.
+ */
+
+
+#define CS PC0                      // Chip select pin, any vacant portpin can be used
+
+//float temp,hum;                     // Variables for the temperature and humidity, which will be recieved.
+byte temp,hum;                     // Variables for the temperature and humidity, which will be recieved.
+
+void initSPI(){
+
+  // initilize the SPI functionalities on the ATmega
+  
+  DDRB  |= 1<<DDB2;                 // SS Pin has to be an output!!!!
+  PORTB |= 1<<PB2;                  // SS Pin set to HIGH so that a connected Pin would deselected
+  
+  
+  SPCR  |= (1<<MSTR);               // Enable Master role
+  SPCR  |= (1<<SPE);                // Enable SPI
+  
+  // To change the clock speed. See datasheet. I had to do this, with full speed I received a lot of wrong data.
+  // Can be slowed down and expedited everywhere in the code. So the communication to slave A could be slow at 16 MHz and the 
+  // communication to slave B fast with 2 MHz:
+  SPCR  |= ((1<<SPR1) | (1<<SPR0));     
+
+  // Define SCK, MOSI and custom CS as output
+  DDRB |= ((1<SS) | (1<<DDB3) | (1<<DDB5));
+
+}
+byte receiveInt(const char reqID){
+  byte i=0;
+  PORTC &= ~(1<<CS); 
+  delayMicroseconds(20);            // Give the slave some time to digest
+
+  
+  // Send out the reqID
+  SPDR = reqID;
+  /* Wait for transmission complete */
+  //while(!(SPSR & (1<<SPIF)))
+  delayMicroseconds(20);           // Give the slave some time to digest
+
+  // Receive 1st byte. The '11' is not considered from the slave, however this step is necessary to initiate the transmission.
+  SPDR = 11;                      
+  /* Wait for transmission complete */
+ // while(!(SPSR & (1<<SPIF)))
+  //i = SPDR;                 // put the byte index 0 of the 'byte array' = float
+  delayMicroseconds(20);
+
+  SPDR = 11;                      
+  /* Wait for transmission complete */
+ // while(!(SPSR & (1<<SPIF)))
+ // i = SPDR;                 // put the byte index 0 of the 'byte array' = float
+  delayMicroseconds(20);
+
+  PORTC |= (1<<CS);             // Set chip select high and stop communication
+
+  return i;
+
+
+}
+bool sendInt(const char reqID, byte b){
+   
+  PORTC &= ~(1<<CS); 
+  delayMicroseconds(20);            // Give the slave some time to digest
+
+  
+  // Send out the reqID
+  SPDR = reqID;
+  /* Wait for transmission complete */
+  while(!(SPSR & (1<<SPIF)))
+  delayMicroseconds(20);           // Give the slave some time to digest
+
+  // Receive 1st byte. The '11' is not considered from the slave, however this step is necessary to initiate the transmission.
+  SPDR = b;                      
+  /* Wait for transmission complete */
+  while(!(SPSR & (1<<SPIF)))
+  //i = SPDR;                 // put the byte index 0 of the 'byte array' = float
+  delayMicroseconds(20);
+ Serial.println(b);
+  PORTC |= (1<<CS);             // Set chip select high and stop communication
+
+   
+
+
+}
+
+float receiveFloat(const char reqID){
+
+  float ans=0;                      // target variable for requested signal
+  byte* ansPTR = (byte*)&ans;       // byte pointer on the target variable
+
+  // Pull chip select pin down to tell slave that the communications starts
+  PORTC &= ~(1<<CS); 
+  delayMicroseconds(20);            // Give the slave some time to digest
+
+  
+  // Send out the reqID
+  SPDR = reqID;
+  /* Wait for transmission complete */
+  while(!(SPSR & (1<<SPIF)))
+  delayMicroseconds(20);           // Give the slave some time to digest
+
+  // Receive 1st byte. The '11' is not considered from the slave, however this step is necessary to initiate the transmission.
+  SPDR = 11;                      
+  /* Wait for transmission complete */
+  while(!(SPSR & (1<<SPIF)))
+  ansPTR[0] = SPDR;                 // put the byte index 0 of the 'byte array' = float
+  delayMicroseconds(20);
+
+  
+  // Receive 2nd byte
+  SPDR = 11;
+  /* Wait for transmission complete */
+  while(!(SPSR & (1<<SPIF)))
+  ansPTR[1] = SPDR; 
+  delayMicroseconds(20);  
+
+  // Receive 3rd byte
+  SPDR = 11;
+  /* Wait for transmission complete */
+  while(!(SPSR & (1<<SPIF)))
+  ansPTR[2] = SPDR; 
+  delayMicroseconds(20);  
+
+  // Receive 4th byte
+  SPDR = 128;
+  /* Wait for transmission complete */
+  while(!(SPSR & (1<<SPIF)))
+  ansPTR[3] = SPDR; 
+  delayMicroseconds(20);    
+  
+
+  PORTC |= (1<<CS);             // Set chip select high and stop communication
+
+  return ans;
+}
+
+void setup (void)
+{
+  Serial.begin(115200);         // Serial transmission for demonstration puroses
+  
+  initSPI();
+  DDRC |= (1<<DDC0);            // datadrection for chip select is not in initSPI()
+}  
+byte a=120;
+
+void loop (void)
+{
+  sendInt('L',a);
+  a++;
+
+  //temp = receiveInt('T');     // See function definition
+  //delay(100);
+  //hum = receiveInt('H');
+  ///delay(100);
+
+  // Print in serial monitor for demonstration
+  //Serial.print("T = ");
+ // Serial.print(temp, DEC);
+  //Serial.println();
+
+  //Serial.print("H = ");
+ // Serial.print(hum, DEC);
+  //Serial.println(); 
+
+  delay(10);
+  
+}  // end of loop
---
+++ USI_Slave/USI_Slave.ino
@@ -0,0 +1,143 @@
+/*  USI SPI Slave Demo written by Robert Mollik, 2016
+ *  
+ *  This demo uses the SPI protocol with USI interface. This is necessary with with the smaller AVR controller since they don't 
+ *  a complete SPI functionality. 
+ *  Demo is written for the pinout of an Arduino Trinket and ATtiny85. Pins need to be modified if a different controller is used.
+ *  
+ *  The demo takes receives a request ID from the Master at the beginning of every communication. According to the request ID,
+ *  it sends back either a temperature or a humidity values. Since both values are present in float, the 4 bytes of the float need
+ *  to be send separately.
+ */
+
+#define CS    PA1 //PB3     // Chip select
+#define LED   PA7 //PB4     // Test LED
+#define DO    PA5     // MISO or Data Out
+#define USCK  PA4     // Clock
+
+// Variable use in Interrupt need to be volotile to tell the compiler, they may be altered in other functions than in Main loop.
+// Otherwise the compiler could remove or replace them with a constant:
+
+volatile char reqID = 0;              // This is for the first byte we receive, which is intended to be the request identifier
+volatile uint8_t index = 0;           // this is to send back the right element in the array
+volatile char ana = 0;  
+// Test variables:
+
+volatile byte temperature = 10;
+volatile byte humidity = 20;
+
+// Pointer on the test variables. Datatype is casted into byte to read out the single bytes of the float:
+
+byte * tempPTR = (byte*) &temperature;
+byte * humPTR  = (byte*) &humidity;
+
+void setup (void)
+{
+  cli();                              // Deactivate Interrupts
+  pinMode(LED, OUTPUT);
+  DDRA |= 1<<LED;                     // Set PB1 as output (LED on Trinket) / PB0 will be the Input for the Interrupt
+  DDRA |= 1<<DO;                      // MISO Pin has to be an output
+
+  USICR = ((1<<USIWM0)|(1<<USICS1));  // Activate 3- Wire Mode and use of external clock but NOT the interrupt at the Counter overflow (USIOIE)
+  
+  PORTA |= 0<<LED;                    // Turn PB1 off
+  PORTA |= 1<<CS;                     // Activate Pull-Up resistor on PB0
+  
+ // PCMSK1|=1<<CS;                       // Active Interrupt on PB1
+  //GIMSK|=1<<PCIE;                     // General Interrupt Mask Register / PCIE bit activates external interrupts
+//GIMSK  = 0b00010000;  
+  GIMSK = (1 << PCIE0);
+    PCMSK0 = (1 << CS);
+
+  delay(500);
+  //digitalWrite(LED, HIGH);
+  sei();         
+  //PORTA^= 1<<LED;    
+  analogWrite(LED,10);                  
+}  // end of setup
+
+
+// Interrupt routine at the CS pin. Always executed when value on CS pin changes:
+
+ISR(PCINT0_vect)
+  {    
+    if((PINA & (1<<CS))== 0){
+      
+      // If edge is falling, the command and index variables shall be initialized
+      // and the 4-bit overflow counter of the USI communication shall be activated:
+
+      reqID = 0;
+      index = 0;
+      USICR |= (1<<USIOIE);
+      USISR = 1<<USIOIF;      // Clear Overflow bit
+
+      // XOR operation to turn PB3 (LED) on/off. (Not needed for USI)
+      
+      //PORTA^= 1<<LED;  
+    }
+    else{
+
+      // If edge is rising, turn the 4-bit overflow interrupt off:
+      
+      USICR &= ~(1<<USIOIE);
+    }
+  }
+
+ // USI interrupt routine. Always executed when 4-bit overflows (after 16 clock edges = 8 clock cycles):
+
+ISR(USI_OVF_vect)
+  {  
+    
+      switch(reqID){
+
+        // Switch-Case to respond according to request from Master:
+
+        case 0:               // If reqID value is zero (just initialized), then first message is the reqID.
+          temperature++; 
+          //PORTA^= 1<<LED; 
+          reqID = USIDR;      // Read in from USIDR register
+          USISR = 1<<USIOIF;  // Clear Overflow bit
+          break;
+        case 'L': {
+            ana = USIDR;      // Read in from USIDR register
+             analogWrite(LED,ana);
+            USISR = 1<<USIOIF;  // Clear Overflow bit
+            reqID=0;
+            break;
+
+        }
+        case 'M': {
+          analogWrite(LED,ana);
+          reqID=0;
+          break;
+
+        }
+        case 'T':
+
+          // Write value to send back into USIDR and clear the overflow bit:
+          USIDR = temperature;//tempPTR[index];
+          USISR = 1<<USIOIF;      
+           
+          index++;            // Increment index to transmit the folloing element next
+          break;
+          
+        case 'H':
+        
+          // Write value to send back into USIDR and clear the overflow bit:
+          USIDR = humidity;//humPTR[index]; 
+          USISR = 1<<USIOIF; 
+          //humidity++;
+          index++;            // Increment index to transmit the folloing element next
+          break;
+
+        default:
+          // Default option of Switch-Case. Send 'reqID' back for debugging.
+          USIDR = reqID;
+          USISR = 1<<USIOIF;
+          
+      }      
+  }
+
+void loop (void)
+{
+  // Nothing done here in this example
+}  
